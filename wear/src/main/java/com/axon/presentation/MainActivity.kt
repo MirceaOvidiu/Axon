968d43732a69c013f2143d0fd40715edd020af71
MirceaOvidiu <mircea.ovidiu.03@gmail.com>
2025-11-04 21:35:12 +0200

    added mock senzor connectivity

--- wear/src/main/java/com/axon/presentation/MainActivity.kt
+++ wear/src/main/java/com/axon/presentation/MainActivity.kt
@@ -8,25 +8,28 @@ package com.axon.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
-import androidx.wear.compose.material.TimeText
 import androidx.wear.tooling.preview.devices.WearDevices
-import com.axon.R
 import com.axon.presentation.theme.AxonTheme
+import com.axon.senzors.SensorViewModel
 
 class MainActivity : ComponentActivity() {
+    private val sensorViewModel: SensorViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
 
@@ -35,38 +38,93 @@ class MainActivity : ComponentActivity() {
         setTheme(android.R.style.Theme_DeviceDefault)
 
         setContent {
-            WearApp("Android")
+            WearApp(sensorViewModel)
         }
     }
 }
 
 @Composable
-fun WearApp(greetingName: String) {
+fun WearApp(sensorViewModel: SensorViewModel) {
+    val gyroscopeData by sensorViewModel.gyroscopeData.collectAsState()
+    val heartRateData by sensorViewModel.heartRateData.collectAsState()
+
     AxonTheme {
-        Box(
+        Column(
             modifier = Modifier
                 .fillMaxSize()
                 .background(MaterialTheme.colors.background),
-            contentAlignment = Alignment.Center
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            TimeText()
-            Greeting(greetingName = greetingName)
+            Text(
+                text = "Gyroscope (rad/s):",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.title3
+            )
+            Text(
+                text = "X: %.2f".format(gyroscopeData[0]),
+                textAlign = TextAlign.Center,
+            )
+            Text(
+                text = "Y: %.2f".format(gyroscopeData[1]),
+                textAlign = TextAlign.Center,
+            )
+            Text(
+                text = "Z: %.2f".format(gyroscopeData[2]),
+                textAlign = TextAlign.Center,
+            )
+
+            Text(
+                text = "Heart Rate:",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.title3
+            )
+            Text(
+                text = "%.1f BPM".format(heartRateData),
+                textAlign = TextAlign.Center,
+            )
         }
     }
-}
-
-@Composable
-fun Greeting(greetingName: String) {
-    Text(
-        modifier = Modifier.fillMaxWidth(),
-        textAlign = TextAlign.Center,
-        color = MaterialTheme.colors.primary,
-        text = stringResource(R.string.hello_world, greetingName)
-    )
 }
 
 @Preview(device = WearDevices.SMALL_ROUND, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
-    WearApp("Preview Android")
+    AxonTheme {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colors.background),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Gyroscope (rad/s):",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.title3
+            )
+            Text(
+                text = "X: 0.00",
+                textAlign = TextAlign.Center,
+            )
+            Text(
+                text = "Y: 0.00",
+                textAlign = TextAlign.Center,
+            )
+            Text(
+                text = "Z: 0.00",
+                textAlign = TextAlign.Center,
+            )
+
+            Text(
+                text = "Heart Rate:",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.title3
+            )
+            Text(
+                text = "0.0 BPM",
+                textAlign = TextAlign.Center,
+            )
+        }
+    }
 }
